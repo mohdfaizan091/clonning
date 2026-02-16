0483302bcdfee64e38a5910d3da3886ded1c07fb
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-02-17 00:56:10 +0530

    basic frontend set-up

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const LandingPage = () => {
+	return (
+		<div className="landing-container">
+			<header className="landing-header">
+				<h1>Welcome to My App</h1>
+			</header>
+		</div>
+	);
+};
+
+export default LandingPage;
