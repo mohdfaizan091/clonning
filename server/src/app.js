ff43a9cb656e684849b2f98b572ca27d96425959
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-02-17 21:57:46 +0530

    install other dependency

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,23 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    })
+);
 
 //health route
 app.get("/home" , (req, res) => {
-    res.send("server is health");
+    res.status(200).json({ message: "ok" });
 })
 
 //export app
